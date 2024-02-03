3d3d71ca5aea08d4464ccfb507ce21311690321b
Djihane <bouizemdjihane1312@gmail.com>
2024-02-03 13:25:21 +0100

    First Imput Button Component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,21 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { InputButtonUnitComponent } from './input-button-unit/input-button-unit.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  imports: [CommonModule, InputButtonUnitComponent],
+//   template: `
+//   <h1>
+//   Welcome to {{ title }}!
+// </h1>`,
+templateUrl : 'app.component.html',
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'todo-list';
+  name = "takima";
 }
+
