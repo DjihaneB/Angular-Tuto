0d7b07631a8635a1c64ced5b09c67464aa03999c
Djihane <bouizemdjihane1312@gmail.com>
2024-02-20 00:44:43 +0100

    Local storage *

---
+++ src/app/services/storage.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class StorageService {
+
+  // getData(key: string):any{
+  //   return JSON.parse(localStorage.getItem(key));
+  // }
+  getData(key: string): any {
+    const item = localStorage.getItem(key);
+    if (item) {
+      return JSON.parse(item);
+    }
+    return null; // or whatever default value you want to return when the item is not found
+  }
+
+  setData(key: string, data: any) {
+    localStorage.setItem(key, JSON.stringify(data));
+  }
+  constructor() { }
+}
